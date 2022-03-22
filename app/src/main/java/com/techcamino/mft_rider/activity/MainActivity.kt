8f728dde4cdf56e627140e7f44334b6276d6ae68
avinashk369 <avinash.k369@gmail.com>
2022-03-22 22:21:40 +0530

    github

--- app/src/main/java/com/techcamino/mft_rider/activity/MainActivity.kt
+++ app/src/main/java/com/techcamino/mft_rider/activity/MainActivity.kt
@@ -1,35 +1,32 @@
 package com.techcamino.mft_rider.activity
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.graphics.Bitmap
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import com.google.gson.Gson
 import com.techcamino.mft_rider.R
 import com.techcamino.mft_rider.apis.ApiClient
 import com.techcamino.mft_rider.apis.ApiInterface
+import com.techcamino.mft_rider.models.MessageDetail
 import com.techcamino.mft_rider.models.UserModel
 import kotlinx.coroutines.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import com.google.gson.Gson
-import com.techcamino.mft_rider.models.MessageDetail
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.io.File
 import java.io.IOException
-import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
 
-
+// github repo
 class MainActivity : AppCompatActivity() {
     lateinit var apiService: ApiInterface
     private var coroutineJob: Job? = null
